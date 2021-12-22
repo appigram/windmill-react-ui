0ddf890d877385fa0757b138acc9302830ada030
Evgeny Sysmanov <appigram@gmail.com>
2021-12-22 22:47:33 +0300

    remoe dist and lib folders from gitignore

---
+++ dist/DropdownItem.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { ButtonProps } from './Button';
+declare const DropdownItem: React.ForwardRefExoticComponent<ButtonProps & React.RefAttributes<React.ForwardRefExoticComponent<ButtonProps & React.RefAttributes<React.ReactNode | HTMLElement>>>>;
+export default DropdownItem;
+//# sourceMappingURL=DropdownItem.d.ts.map
